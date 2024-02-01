c9a096ef0bdc01dbc6a9104198dbe0e7f43337b1
Bashi <venevesheh@gmail.com>
2024-02-01 10:02:14 +0330

    base service impel created

---
+++ src/main/java/base/service/BaseServiceImpel.java
@@ -0,0 +1,26 @@
+package base.service;
+
+import base.model.BaseEntity;
+import base.repository.BaseRepository;
+
+import java.io.Serializable;
+
+public class BaseServiceImpel <ID extends Serializable, TYPE extends BaseEntity<ID>,
+        R extends BaseRepository<ID, TYPE>> implements BaseService<ID,TYPE> {
+    private final R repository;
+
+    public BaseServiceImpel(R repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void save(TYPE entity) {
+        repository.save(entity);
+
+    }
+
+    @Override
+    public TYPE findById(ID id) {
+        return null;
+    }
+}
