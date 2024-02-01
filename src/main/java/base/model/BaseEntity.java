0fd190b55da1059a9f5c7af2daee41482d5d9c75
Bashi <venevesheh@gmail.com>
2024-02-01 09:31:39 +0330

    Base entity commited

--- src/main/java/base/model/BaseEntity.java
+++ src/main/java/base/model/BaseEntity.java
@@ -1,4 +1,15 @@
 package base.model;
 
-public class BaseEntity {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseEntity <ID extends Serializable> {
+    private ID id;
 }
